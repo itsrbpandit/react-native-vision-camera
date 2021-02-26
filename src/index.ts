134ec150e053f1107e2c22794a122f122207a28f
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-26 19:44:22 +0100

    Export frame processor funcs

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,15 @@ export * from './CameraDevice';
 export * from './CameraError';
 export * from './CameraPosition';
 export * from './CameraPreset';
+export * from './FrameProcessor';
 export * from './PhotoFile';
 export * from './Point';
 export * from './Snapshot';
 export * from './TemporaryFile';
 export * from './VideoFile';
+
 export * from './hooks/useCameraDevices';
 export * from './hooks/useCameraFormat';
+export * from './hooks/useFrameProcessor';
+
 export * from './utils/FormatFilter';
